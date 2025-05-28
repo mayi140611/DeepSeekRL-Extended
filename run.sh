9c2303190d8d11590d69e03cda106de564841d4e
mayi140611 <yongguiluo@hotmail.com>
2025-05-28 08:10:24 +0000

    sm

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # python main.py --output_dir "final1" --verbose
+nohup python main.py --model_name Qwen/Qwen2.5-1.5B-Instruct --temperature 0.7     --dataset_name gsm8k --evaluator gsm8k --output_dir "final_gsm8k_qwen15_t07_chain8_reason_len_reward" --num_chains 8 --verbose     > a.log &
 python plotter.py --log_dir "final1"
